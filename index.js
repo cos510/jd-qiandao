e5a287e6b70bd8f631e1ff6d260498f64b83157b
lixiao1 <76551155+cos510@users.noreply.github.com>
2021-01-15 15:50:25 +0800

    Update index.js

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ exports.main_handler = async (event, context, callback) => {
     for (const v of event["Message"].split("&")) {
       console.log(v);
       var request = require('request');
-      //1.执行自己上传的js文件
-      //delete require.cache[require.resolve('./'+v+'.js')];
-      //require('./'+v+'.js')
+      1.执行自己上传的js文件
+      delete require.cache[require.resolve('./'+v+'.js')];
+      require('./'+v+'.js')
 
-      2.执行国内gitee远端js文件如果部署，在国内节点，选择1或2的方式
-      request(https://gitee.com/lxk0301/jd_scripts/tree/master/'+v+'.js', function (error, response, body) {
-      eval(response.body)
-      })
+      //2.执行国内gitee远端js文件如果部署，在国内节点，选择1或2的方式
+      //request(https://gitee.com/lxk0301/jd_scripts/tree/master/'+v+'.js', function (error, response, body) {
+     // eval(response.body)
+     // })
 
       //3.执行github远端的js文件(因github的raw类型的文件被墙,此方法云函数不推荐)
       //request('https://raw.githubusercontent.com/lxk0301/jd_scripts/master/' + v + '.js', function (error, response, body) {
